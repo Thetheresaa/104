7548786bbf57fcd12de126861f2d7a8e67ca11a4
Raffaele Lucci <r.lucci@majorbit.com>
2025-02-07 16:08:07 +0100

    Rework, fix bug, fix sint, fix code

--- Main.java
+++ Main.java
@@ -1,49 +1,49 @@
 import pacchetto.Persona;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args)
 
+	public static void main(String[] args) {
+    	
+		
+	        Scanner input = new Scanner(System.in);
+	        Persona prova = new Persona ("Pippo", "Rossi","scemochilegge1315");
+	   
+		
+	        System.out.println("Cosa vuoi fare?");
+	        System.out.println("Premi 1 per stampare ciao");
+	        System.out.println("Premi 2 per uscire");
 
-    {
-        Scanner input = new Scanner(System.in);
-        Persona prova = new Persona (nome "Pippo", cognome "Rossi", codicefiscale "scemochilegge1315")
-    }
-        System.out.println("Cosa vuoi fare?")
-        System.out.println("Premi 1 per stampare ciao")
-        System.out.println("Premi 2 per uscire")
+	        Integer scelta = null; // La variabile scelt la devi dichiarare
+	        try
+	         {
+	             scelta = input.nextInt();
+	         }catch (InputMismatchException e) {
+	        	 e.printStackTrace();
+	        	 return;
+	         }
+	        
+	        // scelta = input.nextInt(); Questo è inutile
+	       
 
-        try
-         {
-             scelta = inputi.nextInt();
-         }
-        catch (inputtMismatchException e)
-        scelta = inputi.nextInt();
-        switch(scelta)
+	    boolean check = true;
+	   
+	    switch(scelta) {
+	        case 1:
+	            System.out.println("Ciao");
+	            break;
+	        case 2:
+	            System.out.println("Esco");
+	            break;
+	        case 3:
+	            System.out.println("Hai inserito un valore non valido");
+	            break;
+	    }while(check);
+	    
 
-    boolean check = true;
-    Scanner input = new scanner (System.in)
-    int scelta;
-    do
-    {
-        case 1;
-            System.out.println("Ciao");
-            break;
-        case 2;
-            System.out.println("Esco");
-            break;
-        case 3;
-            System.out.println("Hai inserito un valore non valido");
-            break;
-    }
+	}
 }
-
-while (check);
-
-
-
-
-System.out.print "Stampa data" ---> "+pippo.calcola"
--- pacchetto/Persona.java
+++ pacchetto/Persona.java
@@ -1,127 +1,124 @@
 package pacchetto;
 
-public class Persona
-{
+public class Persona {
 
-        private String nome; no usages
-        private String cognome; no usages
-        private int eta;no usages
-        private String codicefiscale; no usages
-        private String citta: no usages
-        private String giorno
-        private String mese
-        public_Persona(String nome, String cognome, String codicefiscale)
-        {
-            this.nome = nome;
-            this cognome = cognome;
-            this codicefiscale = codicefiscale;
-            this giorno = calcolaNascita();
-        }
-        public String_getNome()
-        {
-            return this.nome;
-        }
-        public void setNome(String nome)
-        {
-            this.nome = nome;
-        }
-        public String_getCognome()
-        {
-        return this.cognome;
-        }
-        public void setCognome(String nome)
-        {
-        this.cognome = cognome;
-        }
-        public int_getEta()
-        {
-            return this.eta;
-        }
-        public void setEta(String nome)
-        {
-            this.eta = eta;
-        }
-        public String_getCodicefiscale()
-        {
-            return this.codicefiscale;
-        }
-        public void setCodicefiscale(String nome)
-        {
-        this.codicefiscale = codicefiscale;
-        }
-        public String_getCitta()
-        {
-            return this.citta;
-        }
-        public void setCitta(String nome)
-        {
-            this.citta = citta;
-        }
-        public void calcolanascita()
-        {
-                booblen check
-                String giorno = codicefiscale.substring (9, 11);
+	private String nome; // no usages
+	private String cognome;// no usages
+	private int eta; // no usages
+	private String codicefiscale;// no usages
+	private String citta; // no usages
+	private String giorno;
+	private String mese;
 
-                int giornoNum = Integer.parseInt(giorno);
-                if(giornoNum > 40)
-                        giornonum -= 40;
-                if(giornoNum > 31 || giornoNum < 0)
-                        return "Data errata"
+	public Persona(String nome, String cognome, String codicefiscale) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codicefiscale = codicefiscale;
+		this.giorno = calcolanascita();
+	}
 
-                giorno = giornoNum + "";
-                if(giornoNum < 10)
-                    giorno = "0" + giornoNum;
+	public String getNome() {
+		return this.nome;
+	}
 
-                int mese  = codicefiscale.substring (8, 9);
-                int anno = codicefiscale.substring (6, 8);
-                switch (mese)
-                {
-                        Case 'A'
-                                mese = "01";
-                                break;
-                        Case 'B'
-                                mese = "02";
-                                break;
-                        Case 'C'
-                                mese = "03";
-                                break;
-                        Case 'D'
-                                mese = "04";
-                                break;
-                        Case 'E'
-                                mese = "05";
-                                break;
-                        Case 'H'
-                                mese = "06";
-                                break;
-                        Case 'L'
-                                mese = "07";
-                                break;
-                        Case 'M'
-                                mese = "08";
-                                break;
-                        Case 'P'
-                                mese = "09";
-                                break;
-                        Case 'R'
-                                mese = "10";
-                                break;
-                        Case 'S'
-                                mese = "11";
-                                break;
-                        Case 'T'
-                                mese = "12";
-                                break;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-                        default;
-                                check = false;
-                                break;
-                }
-                if(check)
-                        return giorno + "/" + mese + "/" anno;7
-                else
-                        returno "Data errata"
+	public String getCognome() {
+		return this.cognome;
+	}
 
-        }
+	public void setCognome(String nome) {
+		this.cognome = cognome;
+	}
+
+	public int getEta() {
+		return this.eta;
+	}
+
+	public void setEta(String nome) {
+		this.eta = eta;
+	}
+
+	public String getCodicefiscale() {
+		return this.codicefiscale;
+	}
+
+	public void setCodicefiscale(String nome) {
+		this.codicefiscale = codicefiscale;
+	}
+
+	public String getCitta() {
+		return this.citta;
+	}
+
+	public void setCitta(String nome) {
+		this.citta = citta;
+	}
+
+	public String calcolanascita() {
+		boolean check = false;
+		String giorno = codicefiscale.substring(9, 11);
+
+		int giornoNum = Integer.parseInt(giorno);
+		if (giornoNum > 40)
+			giornoNum -= 40;
+		if (giornoNum > 31 || giornoNum < 0)
+			return "Data errata";
+
+		giorno = giornoNum + "";
+		if (giornoNum < 10)
+			giorno = "0" + giornoNum;
+
+		String mese = codicefiscale.substring(8, 9);
+		String anno = codicefiscale.substring(6, 8);
+		switch (mese) {
+		case "A":
+			mese = "01";
+			break;
+		case "B":
+			mese = "02";
+			break;
+		case "C":
+			mese = "03";
+			break;
+		case "D":
+			mese = "04";
+			break;
+		case "E":
+			mese = "05";
+			break;
+		case "H":
+			mese = "06";
+			break;
+		case "L":
+			mese = "07";
+			break;
+		case "M":
+			mese = "08";
+			break;
+		case "P":
+			mese = "09";
+			break;
+		case "R":
+			mese = "10";
+			break;
+		case "S":
+			mese = "11";
+			break;
+		case "T":
+			mese = "12";
+			break;
+
+		default:
+			check = false;
+			break;
+		}
+		if (check)
+			return giorno + "/" + mese + "/" + anno;
+		else
+			return "Data errata";
+
+	}
 }
-
